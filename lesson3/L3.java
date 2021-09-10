9e1e9f41fe79d628fa5617d619d2c7cdf98e8cd5
Mansur Ziiatdinov <gltronred@gmail.com>
2021-09-10 14:38:49 +0300

    Суммирование

--- lesson3/L3.java
+++ lesson3/L3.java
@@ -5,22 +5,48 @@ public class L3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        // int n = sc.nextInt();
 
-        // цикл while
-        int i = 1;
-        while (i < n) {
-            // действия
-            i *= 2;
+        // // цикл while
+        // int i = 1;
+        // while (i < n) {
+        //     // действия
+        //     i *= 2;
+        // }
+        // System.out.println(i);
+
+        // // do-while
+        // int k = 1;
+        // do {
+        //     // действия
+        //     k *= 2;
+        // } while (k < n);
+        // System.out.println(k);
+
+        ///////////
+        // Вводятся целые числа, ввод заканчивается,
+        // когда ввели 0
+        // Вывести сумму введенных чисел (которые
+        // введены до 0)
+        //
+        // 1 2 3 0
+        // 6
+        int s = 0;
+        int x = sc.nextInt();
+        while (x != 0) {
+            s += x;
+            x = sc.nextInt();
         }
-        System.out.println(i);
+        System.out.println(s);
 
-        // do-while
-        int k = 1;
+        int t = 0;
+        int y;
         do {
-            // действия
-            k *= 2;
-        } while (k < n);
-        System.out.println(k);
+            y = sc.nextInt();
+            if (y != 0) {
+                t += y;
+            }
+        } while (y != 0);
+        System.out.println(t);
     }
 }
