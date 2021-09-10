e3e49b4d1f8a8a67c91db2a52d1269d716a3f20e
Mansur Ziiatdinov <gltronred@gmail.com>
2021-09-10 15:11:45 +0300

    Collatz

--- lesson3/L3.java
+++ lesson3/L3.java
@@ -31,22 +31,60 @@ public class L3 {
         //
         // 1 2 3 0
         // 6
-        int s = 0;
-        int x = sc.nextInt();
-        while (x != 0) {
-            s += x;
-            x = sc.nextInt();
-        }
-        System.out.println(s);
+        //
+        // int s = 0;
+        // int x = sc.nextInt();
+        // while (x != 0) {
+        //     s += x;
+        //     x = sc.nextInt();
+        // }
+        // System.out.println(s);
 
-        int t = 0;
-        int y;
-        do {
-            y = sc.nextInt();
-            if (y != 0) {
-                t += y;
+        // int t = 0;
+        // int y;
+        // do {
+        //     y = sc.nextInt();
+        //     if (y != 0) {
+        //         t += y;
+        //     }
+        // } while (y != 0);
+        // System.out.println(t);
+
+        //////////////////////////////
+        // Последовательность Collatz:
+        // после числа k идёт:
+        // + 3*k+1, если k - нечётное
+        // + k/2, если k - чётное
+        // Последовательность заканчивается,
+        // когда появляется 1
+        //
+        // 5 -> 16 -> 8 -> 4 -> 2 -> 1
+        //
+        // Вводится число N.
+        // Вывести, сколько чисел и максимальное число
+        // в последовательности, начинающейся с N
+        //
+        // 5
+        // 6 16
+        //
+        // 27
+        // ? ?
+        //
+        int k = sc.nextInt();
+        int n = 1;
+        int max = k;
+        while (k != 1) {
+            n++;
+            // if (max < k) {
+            //     max = k;
+            // }
+            max = Math.max(max, k);
+            if (k % 2 == 0) {
+                k /= 2;
+            } else {
+                k = 3 * k + 1;
             }
-        } while (y != 0);
-        System.out.println(t);
+        }
+        System.out.println(n + " " + max);
     }
 }
