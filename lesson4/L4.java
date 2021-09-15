002cc632b384506e8147f9f71ef21228a4741f2b
Mansur Ziiatdinov <gltronred@gmail.com>
2021-09-15 15:53:35 +0300

    Задание: факториал

---
+++ lesson4/L4.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class L4 {
+    public static void main(String[] args) {
+        for (int i=0; i<5; i++) {
+            System.out.println(i);
+        }
+
+        // Ввести неотрицательное N.
+        // Вывести факториал N!
+        // N! = 1*2*...*N
+        // 0! = 1
+        // 3! = 1*2*3 = 6
+    }
+}
