8e0c6091cba821492856386490328b7248b791c1
Mansur Ziiatdinov <gltronred@gmail.com>
2021-09-29 15:48:00 +0300

    Задание

---
+++ lesson7/L7.java
@@ -0,0 +1,12 @@
+
+import java.io.*;
+import java.util.*;
+
+public class L7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Вводятся целые числа, ввод заканчивается нулем.
+        // Вывести сумму всех простых чисел среди них
+    }
+}
