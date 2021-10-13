5c2ff13e5385d0eb6cff98384daa31ffc3c17ba9
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-13 16:03:30 +0300

    Двумерные массивы

--- lesson8/L8.java
+++ lesson8/L8.java
@@ -3,6 +3,30 @@ import java.util.*;
 
 public class L8 {
     public static void main(String[] args) {
-        System.out.println("Arrays");
+        int n = 4;
+        int[][] a = new int[n][];
+        for (int i=0; i<n; i++) {
+            a[i] = new int[1+i];
+            for (int j=0; j<i+1; j++) {
+                a[i][j] = i+j;
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+            System.out.println(Arrays.toString(a[i]));
+        }
+
+        ///////////////////////////////
+        int m = 3;
+        int[][] b = new int[n][m];
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                b[i][j] = i*j;
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+            System.out.println(Arrays.toString(b[i]));
+        }
     }
 }
