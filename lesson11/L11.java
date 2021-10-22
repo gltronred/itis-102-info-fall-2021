a2e448b3f74c8690f2de1d3060f8b4ad430a57e7
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-22 14:49:21 +0300

    Заготовка проверки на простоту

--- lesson11/L11.java
+++ lesson11/L11.java
@@ -1,48 +1,57 @@
 import java.util.*;
 
 public class L11 {
-    public static int twice(int x) {
-        return x*2;
+    // public static int twice(int x) {
+    //     return x*2;
+    // }
+    public static boolean isPrime(int n) {
+        return false;
     }
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
+        System.out.println(isPrime(1));
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(15));
+        System.out.println(isPrime(121));
+        System.out.println(isPrime(1024));
 
-        System.out.println(twice(2));
-        int x = 123;
-        int y = 3;
-        int z = twice(y+1) + 17;
-        System.out.println(z);
+        // System.out.println(Arrays.toString(args));
 
-        int[] a = new int[5];
-        strangeSupreme(a);
-        System.out.println(Arrays.toString(a));
-        strange(a);
-        System.out.println(Arrays.toString(a));
+        // System.out.println(twice(2));
+        // int x = 123;
+        // int y = 3;
+        // int z = twice(y+1) + 17;
+        // System.out.println(z);
 
-        System.out.println(Arrays.toString(input()));
+        // int[] a = new int[5];
+        // strangeSupreme(a);
+        // System.out.println(Arrays.toString(a));
+        // strange(a);
+        // System.out.println(Arrays.toString(a));
 
-        System.out.println(add(x,3));
+        // System.out.println(Arrays.toString(input()));
 
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println(hello(name));
+        // System.out.println(add(x,3));
+
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(hello(name));
     }
-    public static void strangeSupreme(int[] x) {
-        x = new int[3];
-        System.out.println(Arrays.toString(x));
-    }
-    public static void strange(int[] x) {
-        for (int i=0; i<x.length; i++) {
-            x[i] = i+1;
-        }
-    }
-    public static int[] input() {
-        return new int[4];
-    }
-    public static int add(int x, int y) {
-        return x+y;
-    }
-    public static String hello(String name) {
-        return "Hello, " + name;
-    }
+    // public static void strangeSupreme(int[] x) {
+    //     x = new int[3];
+    //     System.out.println(Arrays.toString(x));
+    // }
+    // public static void strange(int[] x) {
+    //     for (int i=0; i<x.length; i++) {
+    //         x[i] = i+1;
+    //     }
+    // }
+    // public static int[] input() {
+    //     return new int[4];
+    // }
+    // public static int add(int x, int y) {
+    //     return x+y;
+    // }
+    // public static String hello(String name) {
+    //     return "Hello, " + name;
+    // }
 }
