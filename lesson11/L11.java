a7d43de31a16bef5f4bacf712b59c4a4e8315e2c
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-22 14:44:44 +0300

    Функции

---
+++ lesson11/L11.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class L11 {
+    public static int twice(int x) {
+        return x*2;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+
+        System.out.println(twice(2));
+        int x = 123;
+        int y = 3;
+        int z = twice(y+1) + 17;
+        System.out.println(z);
+
+        int[] a = new int[5];
+        strangeSupreme(a);
+        System.out.println(Arrays.toString(a));
+        strange(a);
+        System.out.println(Arrays.toString(a));
+
+        System.out.println(Arrays.toString(input()));
+
+        System.out.println(add(x,3));
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(hello(name));
+    }
+    public static void strangeSupreme(int[] x) {
+        x = new int[3];
+        System.out.println(Arrays.toString(x));
+    }
+    public static void strange(int[] x) {
+        for (int i=0; i<x.length; i++) {
+            x[i] = i+1;
+        }
+    }
+    public static int[] input() {
+        return new int[4];
+    }
+    public static int add(int x, int y) {
+        return x+y;
+    }
+    public static String hello(String name) {
+        return "Hello, " + name;
+    }
+}
