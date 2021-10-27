9d94b46b937d8d683e9ab9c9d1ebfcc328a73bd1
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-27 15:57:19 +0300

    Заготовка

---
+++ lesson12/L12.java
@@ -0,0 +1,8 @@
+public class L12 {
+    public static int[] addWithCarry(int q, int[] a, int[] b) {
+        return null;
+    }
+    public static void main(String[] args) {
+        System.out.println(addWithCarry(10, new int[]{1,2,3}, new int[]{9,9,9})); // 0,2,3,1
+    }
+}
