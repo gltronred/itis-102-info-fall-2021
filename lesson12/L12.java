48194ade7e63e3f59df82cbf0b6342a614508c5c
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-27 15:58:50 +0300

    fix

--- lesson12/L12.java
+++ lesson12/L12.java
@@ -1,8 +1,10 @@
+import java.util.*;
+
 public class L12 {
     public static int[] addWithCarry(int q, int[] a, int[] b) {
         return null;
     }
     public static void main(String[] args) {
-        System.out.println(addWithCarry(10, new int[]{1,2,3}, new int[]{9,9,9})); // 0,2,3,1
+        System.out.println(Arrays.toString(addWithCarry(10, new int[]{1,2,3}, new int[]{9,9,9}))); // 0,2,3,1
     }
 }
