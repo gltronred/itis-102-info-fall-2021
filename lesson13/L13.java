27c2475f5df317d427c653e6d0c05264b4c8d90d
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-10 15:55:06 +0300

    Сортировки: начало

---
+++ lesson13/L13.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class L13 {
+    public static void insertion(int[] a) {
+
+    }
+    public static void main(String[] args) {
+        int[] a = new int[]{5,3,2,4,1};
+        insertion(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
