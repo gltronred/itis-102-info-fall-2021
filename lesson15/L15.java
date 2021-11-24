c977c9d9770bc38ad9f9d85ac6dbce72cd1ffe14
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-24 16:34:06 +0300

    Abstract class

--- lesson15/L15.java
+++ lesson15/L15.java
@@ -15,26 +15,47 @@ class Candle implements ILight {
         this.state = state;
     }
 }
-class Lamp implements ILight {
-    int state;
-    public boolean isOn() { return state>0; }
-    public void turnOn() { state=100; }
-    public void turnOff() { state=0; }
-    public Lamp(int state) {
-        this.state = state;
-    }
+abstract class Lamp implements ILight {
+    public abstract int getState();
+    public abstract void setState(int state);
+
+    public boolean isOn() { return getState()>0; }
+    public void turnOn() { setState(100); }
+    public void turnOff() { setState(0); }
 }
 class RGBLamp extends Lamp {
-    public RGBLamp(int state) { super(state); }
+    int r,g,b;
+    public RGBLamp(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+    public int getState() { return r+g+b; }
+    public void setState(int state) {
+        r = state/3;
+        g = state/3;
+        b = state/3;
+    }
+}
+class WhiteLamp extends Lamp {
+    int state;
+    public int getState() { return state; }
+    public void setState(int state) {
+        this.state=state;
+    }
 }
 
 public class L15 {
     public static void main(String[] args){
-        ILight light1 = new Lamp(255);
+        ILight light1 = new WhiteLamp();
         ILight light2 = new Candle(true);
-        ILight light3 = new RGBLamp(123);
+        ILight light3 = new RGBLamp(1,2,3);
         System.out.println(light1.isOn());
         System.out.println(light2.isOn());
         System.out.println(light3.isOn());
+        Lamp l = new WhiteLamp();
+        System.out.println(l);
+        l = new RGBLamp(1,2,3);
+        System.out.println(l);
     }
 }
