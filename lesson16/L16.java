2d6cbcf0290d0109d87c574f0a5ce7818c3dce0b
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-26 20:41:38 +0300

    Разные упражнения со static и final

--- lesson16/L16.java
+++ lesson16/L16.java
@@ -2,20 +2,27 @@ import java.io.*;
 import java.util.*;
 
 class Lamp {
+    public static final String NAME = "Lamp";
+    public final String name;
+
     static int count;
     public static int getCount() {
         return count;
     }
-    public static String getName() {
-        return "Lamp";
+    public static final String getName() {
+        return NAME;
     }
 
     int serial;
     public int getSerial() { return serial; }
     public Lamp() {
         this.serial = count;
+        name = "Lamp " + serial;
         count++;
     }
+}
+
+class RGBLamp extends Lamp {
 }
 
 class OtherLamp {
@@ -44,10 +51,20 @@ public class L16 {
         Lamp l3 = new Lamp();
         System.out.println(l3.getSerial() +
                            " " + l3.getCount());
+
+        System.out.println(l1.name + " " +
+                           l2.name + " " +
+                           l3.name);
+
         System.out.println(l1.getSerial() +
                            " " + l1.getCount());
         System.out.println(Lamp.getCount());
+
+        System.out.println(Lamp.getName());
+        System.out.println(Lamp.NAME);
+
         System.out.println(Math.sin(1));
+
         OtherLamp o1 = OtherLampFactory.makeNewLamp();
         OtherLamp o2 = OtherLampFactory.makeNewLamp();
         OtherLamp o3 = OtherLampFactory.makeNewLamp();
