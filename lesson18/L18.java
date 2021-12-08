137bfb4993fed80bb18409d2a4d4c9eeec222111
Mansur Ziiatdinov <gltronred@gmail.com>
2021-12-08 16:21:33 +0300

    Our exception is simply class

--- lesson18/L18.java
+++ lesson18/L18.java
@@ -10,13 +10,23 @@ import java.util.*;
 // - throw e - выбрасывает ошибку
 // - try ... catch - обрабатывает ошибку
 
-class SomethingWrongException extends RuntimeException {
+class SomethingWrongException extends Exception {
+    private int x;
+    public SomethingWrongException(int x) {
+        this.x = x;
+    }
+    public int getX() { return x; }
+    public String toString() {
+        return "Something went wrong: " + x;
+    }
 }
 
 public class L18 {
     public static void someMethod(int x) {
         if (x == 5) {
-            throw new SomethingWrongException();
+            throw new SomethingWrongException(x);
+        } else if (x == 10) {
+            throw new RuntimeException("Wrong x!");
         }
         System.out.println(x*2);
     }
@@ -26,10 +36,14 @@ public class L18 {
         do {
             try {
                 someMethod(x);
-            } catch (Exception e) {
-                System.out.println("Can't use that x!");
+            } catch (SomethingWrongException e) {
+                System.out.println("Can't use that x: " + e.getX());
                 System.out.println(e);
             }
+            // catch (Exception e) {
+            //     System.out.println("NOOO!!!");
+            //     System.out.println(e);
+            // }
             x = sc.nextInt();
         } while (x != 0);
     }
